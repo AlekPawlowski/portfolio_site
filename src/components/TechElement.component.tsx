31e324ffac7b83af725ce996a0fb9bf5f0c501e2
Aleksander Pawłowski <apawlowski@fly.pl>
2023-04-19 17:29:55 +0200

    tech elements update

--- src/components/TechElement.component.tsx
+++ src/components/TechElement.component.tsx
@@ -1,11 +1,17 @@
-const mainSkils = [
-    {
-        name: "Front-end",
-        maestry: 60
-    },
+interface MainSkils {
+    name: string,
+    maestry: number
+}
+
+const mainSkils: MainSkils[] = [
+    
     {
         name: "JavaScript",
         maestry: 80,
+    },
+    {
+        name: "Typescript",
+        maestry: 55
     },
     {
         name: "CSS",
@@ -16,12 +22,33 @@ const mainSkils = [
         maestry: 80
     },
     {
-        name: "Git",
-        maestry: 40
+        name: "Front-end",
+        maestry: 65
     },
-    
+    {
+        name: "React",
+        maestry: 20
+    },
+    {
+        name: "Gulp",
+        maestry: 50
+    },
+    {
+        name: "JQuery",
+        maestry: 55
+    },
+    {
+        name: "Cypress",
+        maestry: 30
+    },
+    {
+        name: "Jest js",
+        maestry: 30
+    }
 ]
-const additionalSkils = [];
+
+const additionalSkils: string[] = ["REST", "ES6", "Jira", "Nunjucks", "Git", "Adobe Photoshop/Ilustrator/XD", "Accessibility", "Unit Test", "Agile", "OOP", "TDD", ];
+
 export default function TechElement() {
     return <section></section>
 }
