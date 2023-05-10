6ba9efb8c46f5920d93a2c980cefec7164c896d2
Aleksander Pawłowski <apawlowski@fly.pl>
2023-05-10 17:37:10 +0200

    fix navigation

--- src/views/Menu/Menu.menu.tsx
+++ src/views/Menu/Menu.menu.tsx
@@ -5,13 +5,15 @@ import { MenuElement } from './MenuElement.menu';
 export default function Menu(): JSX.Element {
     const location = useLocation();
     console.log(location);
+    // return (<section id="menu_box">
     return (<Fragment>
-        <nav >
+        <nav>
             <MenuElement name="Home page" path="/" location={location.pathname} />
             <MenuElement name="About me" path="/aboutme" location={location.pathname} />
             <MenuElement name="Tech stack" path="/skils" location={location.pathname} />
             <MenuElement name="Contact" path="/contact" location={location.pathname} />
         </nav>
         <Outlet />
+        {/* </section>) */}
     </Fragment>)
 }
