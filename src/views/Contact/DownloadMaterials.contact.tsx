36949e4792152ed2c627b578ead213ea57e4cce6
AlekPawlowski <alekpawlowski21@gmail.com>
2023-10-17 21:39:30 +0200

    add polish cv

--- src/views/Contact/DownloadMaterials.contact.tsx
+++ src/views/Contact/DownloadMaterials.contact.tsx
@@ -2,5 +2,6 @@ import { DownloadElement } from "./DownloadElement.contact"
 export default function DownloadMaterials () {
     return <section className="download_elements">
         <DownloadElement file="cv_aleksander_pawlowski_english.pdf">English</DownloadElement>
+        <DownloadElement file="cv_aleksander_pawlowski_pol.pdf">Polskie</DownloadElement>
     </section>
 }
