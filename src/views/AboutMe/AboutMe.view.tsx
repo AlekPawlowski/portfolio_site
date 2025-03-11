d1fb36d0f8d3b805d3eb2640f63d986e6a24177b
AlekPawlowski <alekpawlowski21@gmail.com>
2025-03-11 20:37:57 +0100

    update CV

--- src/views/AboutMe/AboutMe.view.tsx
+++ src/views/AboutMe/AboutMe.view.tsx
@@ -7,7 +7,7 @@ export default function AboutMe() {
             <h1>About me</h1>
             {/* <h2></h2> */}
             <p>
-                I am a programmer with over 5 years of experience. My skills include programming in languages such as JavaScript, TypeScript, as well as knowledge in HTML, CSS (SASS/LESS), GIT, and REST. Over the past years, I have worked on various projects as a freelancer and with teams of developers, serving both small clients and international brands such as Huawei, McDonald's, and SEAT. I have gained experience in developing web and mobile applications, content management systems.
+                I am a programmer with over 7 years of experience. My skills include programming in languages such as JavaScript, TypeScript, as well as knowledge in React, HTML, CSS (SASS/LESS), GIT, and REST. Over the past years, I have worked on various projects as a freelancer and with teams of developers, serving both small clients and international brands such as Huawei, McDonald's, and SEAT. I have gained experience in developing web and mobile applications, content management systems.
                 <br />
                 <br />
                 In my work, I always strive to fulfill my responsibilities in a way that ensures that the solutions I deliver are not only functional and user-friendly but also written in a way that maintains code readability for other developers, while ensuring the appropriate performance of the application.
